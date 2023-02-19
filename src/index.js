a1f28fe1a2fb0b574c0b160c62a9a9896b17623e
vipinnagar8700 <vipinnagar8700@gmail.com>
2023-02-19 21:21:03 +0530

    Second Commit

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ const express = require("express"),
 app.set("view engine", "ejs");
 app.use(express.static('public'))
 
-app.get('/', (req, res) => res.send('Hello World'));
+// app.get('/', (req, res) => res.send('Hello World'));
 
-app.get('/home', function(req, res) {
+app.get('/', function(req, res) {
 res.render('index');
 });
 
