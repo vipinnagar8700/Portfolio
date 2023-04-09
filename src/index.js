17c7a40e1cf71f7474f66c67c8b3c69bce77398e
vipinnagar8700 <vipinnagar8700@gmail.com>
2023-04-09 20:31:44 +0530

    complete

--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
 const express = require("express"),
   app = express();
+  app.use(express.static('public'))
+
+// Middlewares
+
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 //setting view engine to ejs
+
 app.set("view engine", "ejs");
-app.use(express.static('public'))
 
 // app.get('/', (req, res) => res.send('Hello World'));
 
+app.get('/', function (req, res) {
+  res.render('login');
+});
 
 
+//  login system
+app.post('/api/signIn', urlencodedParser, function (req, res, next) {
+  var email = req.body.email;
+  var password = req.body.password;
+
+  if (email == "vipin@1.com" && password == "1234") {
+    res.redirect('/home');
+  } else {
+    res.send("invalidcred")
+  }
+});
+
+//  End of login system//
+
+// logout Api
+app.get('/logout', function (req, res) {
+  res.render('Login');
+});
+
+// end of logout api
 app.get('/home', function (req, res) {
   res.render('index');
 });
